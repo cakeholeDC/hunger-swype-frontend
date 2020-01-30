897837c34f3e992d4665a88424b0867d041a9cd8
Kyle <kylepcole@gmail.com>
2020-01-30 13:03:16 -0500

    fetching data

--- src/App.js
+++ src/App.js
@@ -3,17 +3,49 @@ import React from 'react';
 import './App.css';
 import MainContainer from './containers/MainContainer.js'
 import { Route, Switch } from "react-router-dom";
+import { connect } from 'react-redux'
 
+const BASE_URL = "http://localhost:3000"
+const DIETS_URL = `${BASE_URL}/diets`
+const COURSES_URL = `${BASE_URL}/courses`
+const CUISINES_URL = `${BASE_URL}/cuisines`
 
+class App extends React.Component {
+  
+  componentDidMount(){
+    console.log("App =>", this.props)
+    fetch(DIETS_URL)
+      .then(res => res.json())
+      .then(diets_array => this.props.fetchedDiets(diets_array))
+    fetch(CUISINES_URL)
+      .then(res => res.json())
+      .then(cuisines_array => this.props.fetchedCuisines(cuisines_array))
+    fetch(COURSES_URL)
+      .then(res => res.json())
+      .then(courses_array => this.props.fetchedCourses(courses_array))
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/" component={ MainContainer } />
-      </Switch>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/" component={ MainContainer } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUser,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchedDiets: (diets_array) => { dispatch({ type: "FETCHED_DIETS", payload: diets_array }) },
+  fetchedCuisines: (cuisines_array) => { dispatch({ type: "FETCHED_CUISINES", payload: cuisines_array }) },
+  fetchedCourses: (courses_array) => { dispatch({ type: "FETCHED_COURSES", payload: courses_array }) }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,9 @@
 // const START_SWYPE = "START_SWYPE"
 // const FILTER = "FILTER"
 const PROGRESS = "PROGRESS"
+// const FETCHED_DIETS = "FETCHED_DIETS"
+// const FETCHED_CUISINES = "FETCHED_CUISINES"
+// const FETCHED_COURSES = "FETCHED_COURSES"
 
 function home(){
 	return { type: PROGRESS, payload: null}
