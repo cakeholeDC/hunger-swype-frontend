a36ea458395a2e6d2e961950058d53bd0bba8ad1
Kyle <kylepcole@gmail.com>
2020-02-04 20:31:02 -0500

    splash screen implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import MainContainer from './containers/MainContainer.js'
 import Match from './components/Match.js'
 import Recipe from './components/Recipe.js'
+import SplashScreen from './components/SplashScreen.js'
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from 'react-redux'
 import { fetchingDiets, fetchingCuisines, fetchingCourses } from './redux/actions'
@@ -11,8 +12,18 @@ import { fetchingDiets, fetchingCuisines, fetchingCourses } from './redux/action
 
 
 class App extends React.Component {
+  state={
+    loading: true
+  }
+
+  appLoaded = () => {
+    // this.setState({
+    //   loading: false
+    // })
+  }
   
   componentDidMount(){
+    setTimeout( this.appLoaded, 4000);
     this.props.fetchingDiets()
     this.props.fetchingCuisines()
     this.props.fetchingCourses()
@@ -20,16 +31,18 @@ class App extends React.Component {
 
   render(){
     return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/match" render={ (props) => this.props.progress === "match" || this.props.progress === "results" 
-              ? <Match />
-              : <Redirect to='/' />
-          }/>
-          <Route path="/match/recipe/:id" component={Recipe}/>
-          <Route path="/" component={ MainContainer } />
-        </Switch>
-      </div>
+      this.state.loading 
+        ? <SplashScreen />
+        : <div className="App">
+            <Switch>
+              <Route exact path="/match" render={ (props) => this.props.progress === "match" || this.props.progress === "results" 
+                  ? <Match />
+                  : <Redirect to='/' />
+              }/>
+              <Route path="/match/recipe/:id" component={Recipe}/>
+              <Route path="/" component={ MainContainer } />
+            </Switch>
+          </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-import './index.css';
 import 'semantic-ui-css/semantic.min.css'
+import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './redux/store'
