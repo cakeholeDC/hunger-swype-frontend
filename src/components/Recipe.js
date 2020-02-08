d68ba3ebba7c331bc2dee6535a53d1d99dc757e7
Kyle <kylepcole@gmail.com>
2020-02-08 16:15:49 -0500

    started recipe page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -51,55 +51,55 @@ class Recipe extends React.Component {
 			: <React.Fragment>
 				<MenuBar />
 				<div id="recipe-page-container">
-				<Image 
-				 	fluid
-				 	src={ this.state.recipeDetails.photo }
-				 	className="match-image"
-			 	/>
-			 	<Container className="recipe-page">
-					<Header as="h3" className="recipe-title">
-						{ this.state.recipeDetails.title }
-					</Header>
-					<Header as="h4">
-						Cook Time: {this.state.recipeDetails.cook_time} min<br/>Serves: { this.state.recipeDetails.servings }
-					</Header>
-			        <div className="icon-container">
-			        		<small className="favorites-toggle" onClick={ () => this.props.toggleFavorite(this.props.currentUser.id, this.state.recipeDetails) }>
-				          	    <Icon 
-				          	    	color="red" 
-				          	    	name={ this.props.currentUser 
-				          	    			? this.recipeIsFavorite() 
-			          	    					? "heart"
-			          	    					: "heart outline" 
-		          	    					: "heart outline"
-		          	    				 }
-	      	    				/> Flavourite
-			          	    </small>
-						<small className="gray-text recipe-rating">
-							<Icon color='green' name='thumbs up' /> { this.state.recipeDetails.rating }%
-						</small>
-		            </div>
-					<Divider horizontal>
-				      <Header as='h4'>
-				        <Icon name='list' size='mini' />
-							Ingredients 
-					  </Header>
-				    </Divider>
-					<ul className="ingredient-list">
-						{ this.getIngredients().map((ingredient, index) => <li key={`ingredient-${index}`}>{ingredient}</li> ) }
-					</ul>
-					<Divider horizontal>
-				      <Header as='h4'>
-				        <Icon name='tasks' size='mini' />
-							Directions 
-					  </Header>
-				    </Divider>
-				    <ol className="directions-list">
-				    	{ this.state.recipeDetails.directions.map((step, index) => <li key={`step-${index}`}>{step}</li> ) }
-				    </ol>
-				<Divider />
-				<a href={ this.state.recipeDetails.source_url } target="_blank">View Original Recipe</a> 
-				</Container>
+					<Image 
+					 	fluid
+					 	src={ this.state.recipeDetails.photo }
+					 	className="recipe-image"
+				 	/>
+				 	<div className="recipe-page">
+						<Header as="h3" className="recipe-title">
+							{ this.state.recipeDetails.title }
+						</Header>
+						<Header as="h4">
+							Cook Time: {this.state.recipeDetails.cook_time} min<br/>Serves: { this.state.recipeDetails.servings }
+						</Header>
+				        <div className="icon-container">
+				        		<small className="favorites-toggle" onClick={ () => this.props.toggleFavorite(this.props.currentUser.id, this.state.recipeDetails) }>
+					          	    <Icon 
+					          	    	color="red" 
+					          	    	name={ this.props.currentUser 
+					          	    			? this.recipeIsFavorite() 
+				          	    					? "heart"
+				          	    					: "heart outline" 
+			          	    					: "heart outline"
+			          	    				 }
+		      	    				/> Flavourite
+				          	    </small>
+							<small className="gray-text recipe-rating">
+								<Icon color='green' name='thumbs up' /> { this.state.recipeDetails.rating }%
+							</small>
+			            </div>
+						<Divider horizontal>
+					      <Header as='h4'>
+					        <Icon name='list' size='mini' />
+								Ingredients 
+						  </Header>
+					    </Divider>
+						<ul className="ingredient-list">
+							{ this.getIngredients().map((ingredient, index) => <li key={`ingredient-${index}`}>{ingredient}</li> ) }
+						</ul>
+						<Divider horizontal>
+					      <Header as='h4'>
+					        <Icon name='tasks' size='mini' />
+								Directions 
+						  </Header>
+					    </Divider>
+					    <ol className="directions-list">
+					    	{ this.state.recipeDetails.directions.map((step, index) => <li key={`step-${index}`}>{step}</li> ) }
+					    </ol>
+					<Divider />
+					<a href={ this.state.recipeDetails.source_url } target="_blank">View Original Recipe</a> 
+					</div>
 				</div>
 			</React.Fragment>
 		)
