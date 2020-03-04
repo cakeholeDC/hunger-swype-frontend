0617900f87b5d231eab617a09d7d9100d269421a
Kyle <kylepcole@gmail.com>
2020-03-04 14:30:21 -0500

    about page content

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,8 @@ class About extends React.Component{
 					<h3>Hunger Swype is <em>flavour dating.</em></h3>
 					<p>A study by John's Hopkins University <a href="https://clf.jhsph.edu/about-us/news/news-2014/study-suggests-home-cooking-main-ingredient-healthier-diet" target="_blank" rel="noopener noreferrer">suggests</a> that cooking at home is the main ingredient for a healthier life.</p>
 					<p id="about-quote"><em>“When people cook most of their meals at home, they consume fewer carbohydrates, less sugar and less fat than those who cook less or not at all – even if they are not trying to lose weight”</em></p><p id="about-quote-source">-Julia A. Wolfson, MPP<br/><small>Fellow, Johns Hopkins Center for a Livable Future</small></p>
-					<p>Hunger Swype is here to help! We know that when it comes to cooking the problem that most of us face isn't the cooking itself, but rather what to cook. Start out by entering your dietary restrictions, then let us know what you're looking for and Hunger Swype will present you with a series of delicious looking photos matching your flavour profile.</p>
+					<p>We know that when it comes to cooking the problem that most of us face isn't the cooking itself, but rather what to cook. Hunger Swype is here to help!</p>
+					<p>Start out by entering your dietary restrictions, then let us know what you're looking for and Hunger Swype will present you with a series of delicious looking photos matching your flavour profile.</p>
 					<p>Your only job is to decide whether that dish looks appetizing or not and we'll take it from there. Hunger Swype’s flavour algorithm will match you with suggested recipes.</p>
 					<p>No more arguments over what's for dinner. No more frozen pizzas or rotisserie hot-dogs from the bodega around the corner (unless you're into that).</p>
 					<p>Let your appetite drive your flavour profile!</p>
